162c22195fd453fb267c629d818fdd18e0aa9596
Aayush <aayush@Vijays-MacBook-Pro.local>
2019-01-02 13:36:42 +0530

    encodeURI & decodeURI

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -1,7 +1,23 @@
-var request = require('request');
+const request = require('request');
+const yargs = require('yargs');
+const argv = yargs
+    .option({
+        a :{
+            demand : true,
+            alias: 'address',
+            descibe:'Address to fetch weather',
+            string : true
+        }
+    })
+    .help()
+    .alias('help','h')
+    .argv;
+console.log(argv);
 request({
-    url:'www.google.com',
-    json:true
+    url:'https://www.google.com/',
+    json: true
 },(error,response,body)=>{
-    console.log(JSON.stringify(body,undefined,2));
+    //console.log(JSON.stringify(body,undefined,2));
+    //console.log(response);
+    
 })
